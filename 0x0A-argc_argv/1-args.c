ca2cddf96e3d7f9645db505eeb9fefca6407e925
osabikz <osabikz94@gmail.com>
2023-10-02 09:16:17 -0700

    task 1

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * main - program that prints numbr of argument passed into it
+ * @argc: number of argument
+ * @argv: array of argument
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	(void argv; /*ignore argv*/
+	 printf("%d\n", argc - 1);
+
+return (0);
+}
