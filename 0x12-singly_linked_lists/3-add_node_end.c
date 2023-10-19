755c830f7df9d1ad6b0e6631e66906fb63693778
osabikz <osabikz94@gmail.com>
2023-10-19 07:58:18 -0700

    task 3

---
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -0,0 +1,72 @@
+#include "lists.h"
+
+int len(const char *str);
+list_t *create_node(const char *str);
+/**
+ * add_node_end - a function that ad notes
+ * @head: pointer to the forst node
+ * @str: the new node
+ * Return: pointer to the head
+ */
+list_t *add_node_end(list_t **head, const char *str)
+{
+	list_t *tmp;
+	list_t *new_node;
+
+	tmp = *head;
+	if (head == NULL)
+	{
+		return (NULL);
+	}
+		new_node = create_node(str);
+	if (new_node == NULL)
+		return (NULL);
+	if (*head == NULL)
+
+	{
+		*head = new_node;
+		return (*head);
+	}
+	while (tmp->next != NULL)
+		tmp = tmp->next;
+	tmp->next = new_node;
+	return (*head);
+}
+
+/**
+ * create_node - function that declears node
+ * @str: string input
+ * Return: pointer to the malloced memory
+ */
+
+list_t *create_node(const char *str)
+{
+	list_t *new_node;
+
+	new_node = malloc(sizeof(list_t));
+	if (new_node == NULL)
+		return (NULL);
+	new_node->str = strdup(str);
+	new_node->len = len(str);
+	new_node->next = NULL;
+	return (new_node);
+}
+/**
+ * len - length of a string
+ * @str: string whose length is to be found
+ * Return: amount of length
+ */
+int len(const char *str)
+{
+	int i;
+
+	if (str == NULL)
+		return (0);
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		i++;
+	}
+	return (i);
+}
