381a1c2940a39335988123b69fa3c535b13f8493
osabikz <osabikz94@gmail.com>
2023-10-19 08:09:02 -0700

    task 4

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+
+/**
+ * free_list - function that frees a list
+ * @head: pointer of the node
+ * return: head
+ */
+
+void free_list(list_t *head)
+{
+	list_t *temp;
+
+	if (head == NULL)
+		return;
+	while (head->next != NULL)
+	{
+		temp = head->next;
+		free(head->str);
+		free(head);
+		head = temp;
+	}
+	free(head->str);
+	free(head);
+}
