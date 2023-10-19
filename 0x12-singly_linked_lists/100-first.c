4b2081a5d696ecb40f93aeb4287940f70578397d
osabikz <osabikz94@gmail.com>
2023-10-19 08:22:31 -0700

    task 5

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,11 @@
+#include "lists.h"
+void ComeFristbyCTC(void)__attribute__((constructor));
+void ComeFristbyCTC(void);
+/**
+ * ComeFristbyCTC - function that runs before the main function
+ */
+void ComeFristbyCTC(void)
+{
+	printf(" You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -22,6 +22,7 @@ typedef struct list_s
 
 list_t *add_node_end(list_t **head, const char *str);
 void free_list(list_t *head);
+void ComeFristbyCTC(void);
 size_t list_len(const list_t *h);
 list_t *add_node(list_t **head, const char *str);
 int len(const char *str);
