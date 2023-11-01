a38fb3c17ac37166ea7ef9f3a7483981656fae11
osabikz <osabikz94@gmail.com>
2023-11-01 05:15:13 -0700

    task 3

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -41,7 +41,7 @@ int main(int argc, char *argv[])
 {
 	int file_from, file_to, err_close;
 	ssize_t nchars, nwr;
-	char buf[1024];
+	char buff[1024];
 
 	if (argc != 3)
 	{
@@ -57,10 +57,10 @@ int main(int argc, char *argv[])
 	while (nchars == 1024)
 
 	{
-		nchars = read(file_from, buf, 1024);
+		nchars = read(file_from, buff, 1024);
 		if (nchars == -1)
 			error_file(-1, 0, argv);
-		nwr = write(file_to, buf, nchars);
+		nwr = write(file_to, buff, nchars);
 			if (nwr == -1)
 			error_file(0, -1, argv);
 	}
