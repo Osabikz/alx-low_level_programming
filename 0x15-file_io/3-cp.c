5533193d89f342d57185f9e8099aa1d2b62afc10
osabikz <osabikz94@gmail.com>
2023-11-01 05:20:57 -0700

    task 3

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -71,6 +71,7 @@ int main(int argc, char *argv[])
 		exit(100);
 	}
 	err_close = close(file_to);
+	if (err_close == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: can't read close fd %d\n", file_to);
 		exit(100);
